a00b38ae6d7162a52b8bfd251db65a8d08994d0c
Gurinder Singh <gurinder.singh@1e.com>
2017-11-19 10:52:13 +0530

    Added some conversions

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,36 @@ mod errors {
 
 use self::errors::*;
 
+// TODO: Replace all explicit From impls with a rust which takes a conversion multiple and the two types
+// TODO: also add bits, bps, kbps etc
+
 pub struct Bytes(u64);
 pub type B = Bytes;
 
+pub struct kB(f64);
+pub struct MB(f64);
+pub struct GB(f64);
 
-pub struct KiB(u64);
-pub struct MiB(u64);
-pub struct GiB(u64);
+pub struct KiB(f64);
+
+impl From<Bytes> for KiB {
+    fn from(bytes: Bytes) -> Self {
+        KiB(bytes.0 as f64 / 1024.0)
+    }
+}
+
+pub struct MiB(f64);
+
+impl From<Bytes> for MiB {
+    fn from(bytes: Bytes) -> Self {
+        MiB(bytes.0 as f64 / 1048576.0)
+    }
+}
+
+pub struct GiB(f64);
+
+impl From<Bytes> for GiB {
+    fn from(bytes: Bytes) -> Self {
+        GiB(bytes.0 as f64 / 1073741824.0)
+    }
+}
