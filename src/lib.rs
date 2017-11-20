668c705cca5a8560b496d32b5cb4aa1c44e2b2e7
Gurinder Singh <frederick.the.fool@gmail.com>
2017-11-20 09:57:42 +0530

    Added macro for From impls

--- src/lib.rs
+++ src/lib.rs
@@ -6,36 +6,42 @@ mod errors {
 
 use self::errors::*;
 
-// TODO: Replace all explicit From impls with a rust macro which takes a conversion multiple and the two types involved
-// TODO: also add units for bits, bps, kbps etc
+// TODO: Also add units for bits, bps, kbps etc
+
+macro_rules! impl_conv {
+    ($type_from:tt, $type_to:tt, $multiple:expr) => {
+        impl From<$type_from> for $type_to {
+            fn from(from_obj: $type_from) -> Self {
+                $type_to(from_obj.0 as f64 * $multiple)
+            }
+        }       
+    };
+}
 
 pub struct Bytes(pub u64);
 pub type B = Bytes;
 
 pub struct kB(pub f64);
+
+impl_conv!(Bytes, kB, 1.0 / 1000.0);
+
 pub struct MB(pub f64);
+
+impl_conv!(Bytes, MB, 1.0 / 1000_000.0);
+
 pub struct GB(pub f64);
+
+impl_conv!(Bytes, GB, 1.0 / 1000_000_000.0);
+
 
 pub struct KiB(pub f64);
 
-impl From<Bytes> for KiB {
-    fn from(bytes: Bytes) -> Self {
-        KiB(bytes.0 as f64 / 1024.0)
-    }
-}
+impl_conv!(Bytes, KiB, 1.0 / 1024.0);
 
 pub struct MiB(f64);
 
-impl From<Bytes> for MiB {
-    fn from(bytes: Bytes) -> Self {
-        MiB(bytes.0 as f64 / 1048576.0)
-    }
-}
+impl_conv!(Bytes, MiB, 1.0 / 1048_576.0);
 
 pub struct GiB(f64);
 
-impl From<Bytes> for GiB {
-    fn from(bytes: Bytes) -> Self {
-        GiB(bytes.0 as f64 / 1073741824.0)
-    }
-}
+impl_conv!(Bytes, GiB, 1.0 / 1073_741_824.0);
