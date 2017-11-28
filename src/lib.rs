dbc9a712ec9e46b27c617f1ae683f6ad06170941
Gurinder Singh <frederick.the.fool@gmail.com>
2017-11-28 12:04:35 +0530

    Implemented Add for all types

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ mod errors {
 #[macro_use] 
 extern crate serde_derive;
 
+use std::ops::{Add, AddAssign};
 use self::errors::*;
 
 // TODO: Also add units for bits, bps, kbps etc
@@ -28,7 +29,8 @@ macro_rules! impl_cast_to_inner {
     };
 }
 
-// TODO: Find a way to change the second argument of this macro from tt to ty 
+// TODO: Find a way to change arguments of the below macros from tt to ty 
+// TODO: use new_derive crate instead of hand rolling these ops impl macros
 macro_rules! impl_conv {
     ($type_from:ty, $type_to:tt, $multiple:expr) => {
         impl From<$type_from> for $type_to {
@@ -39,45 +41,73 @@ macro_rules! impl_conv {
     };
 }
 
+macro_rules! impl_binary_ops_self {
+    ($type:tt) => {
+        impl Add<$type> for $type {
+            type Output=$type;
+            fn add(self, other: $type) -> Self::Output {
+                $type(self.0 + other.0)
+            }
+        }
+    };
+}
+
+macro_rules! impl_binary_ops {
+    ($lhs:ty, $rhs:ty) => {
+        impl<$lhs, $rhs> Add<$rhs> for $lhs {
+            type Output=$lhs;
+            fn add(self, other: $rhs) -> Self::Output {
+                $type(self.0 + other.0)
+            }
+        }
+    };
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Bytes(pub u64);
 pub type B = Bytes;
 
 impl_cast_to_inner!(Bytes, u64);
+impl_binary_ops_self!(Bytes);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct kB(pub f64);
 
 impl_cast_to_inner!(kB, f64);
 impl_conv!(Bytes, kB, 1_f64 / 1000_f64);
+impl_binary_ops_self!(kB);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MB(pub f64);
 
 impl_cast_to_inner!(MB, f64);
 impl_conv!(Bytes, MB, 1_f64 / 1000_000_f64);
+impl_binary_ops_self!(MB);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GB(pub f64);
 
 impl_cast_to_inner!(GB, f64);
 impl_conv!(Bytes, GB, 1_f64 / 1000_000_000_f64);
+impl_binary_ops_self!(GB);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct KiB(pub f64);
 
 impl_cast_to_inner!(KiB, f64);
 impl_conv!(Bytes, KiB, 1_f64 / 1024_f64);
+impl_binary_ops_self!(KiB);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MiB(f64);
 
 impl_cast_to_inner!(MiB, f64);
 impl_conv!(Bytes, MiB, 1_f64 / 1048_576_f64);
+impl_binary_ops_self!(MiB);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GiB(f64);
 
 impl_cast_to_inner!(GiB, f64);
 impl_conv!(Bytes, GiB, 1_f64 / 1073_741_824_f64);
+impl_binary_ops_self!(GiB);
